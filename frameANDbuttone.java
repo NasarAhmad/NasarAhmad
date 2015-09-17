0f4fabc767eaf69b119acafefb59f51b43cade33
NasarAhmad <nasarahmadaqily1@gmail.com>
2015-09-17 16:22:03 -0700

    make a buttone and a frame

---
+++ frameANDbuttone.java
@@ -0,0 +1,23 @@
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class frameANDbuttone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+   JFrame ahmad = new JFrame();
+   ahmad.getContentPane().setLayout(new BorderLayout());  
+   JButton start = new JButton("salam");
+   JTextField say = new JTextField();
+   ahmad.setSize(300, 500);
+   ahmad.getContentPane().add((start),BorderLayout.SOUTH);
+   ahmad.getContentPane().add(say);
+   
+   ahmad.setVisible(true);
+   ahmad.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
